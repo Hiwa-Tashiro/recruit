47a9a62930c0460531766a070c562586a955310d
Hiwa-Tashiro <htashiro@sisco-consulting.co.jp>
2024-12-03 15:05:34 +0900

    api認証追加

--- src/components/About.js
+++ src/components/About.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { fetchAuthSession } from 'aws-amplify/auth';
 
 function About() {
   const [data, setData] = useState();
-  useEffect(() => {
-    const req = fetch("https://y9zs7kouqi.execute-api.ap-northeast-1.amazonaws.com/dev/recruitlambda1-dev",
-      {
-        method: "GET",
-      }
-    )
-      .then((res) => res.json()) // JSON形式に変換
-      .then((json) => setData(json))
-      .catch((error) => console.log(error)); // エラー発生時に出力
-  }, [])
 
   useEffect(() => {
-    console.log(data);
-  }, [data])
+    const test = async () => {
+      const session = await fetchAuthSession();
+      const token = session?.tokens?.idToken?.toString();
+
+      fetch("https://y9zs7kouqi.execute-api.ap-northeast-1.amazonaws.com/dev/recruitlambda1-dev",
+        {
+          method: "GET",
+          headers: { Authorization: token }
+        }
+      )
+        .then((res) => res.json()) // JSON形式に変換
+        .then((json) => setData(json))
+        .catch((error) => console.log(error)); // エラー発生時に出力
+
+    }
+
+    test();
+  }, [])
 
   return (
     <div>
